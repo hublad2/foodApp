7e648f4d683a19b57450288e3521f1460c2ce8ce
hublad <hublad@wp.pl>
2020-09-21 15:20:29 +0200

    Schedules can be removed

--- controllers/scheduleController.js
+++ controllers/scheduleController.js
@@ -3,6 +3,7 @@ const User = require("../models/user");
 const Schedule = require("../models/schedule");
 const fetch = require("node-fetch");
 const passport = require("passport");
+const { filter } = require("async");
 require("../src/passport");
 
 /* exports.get_schedules = async (req, res, next) => {
@@ -20,6 +21,36 @@ exports.get_user_schedule = [
     try {
       const schedule = await Schedule.find({ author: req.body.userId }).exec();
       res.send(schedule);
+    } catch (err) {
+      return next(err);
+    }
+  },
+];
+
+exports.remove_dateFromSchedule = [
+  passport.authenticate("jwt", { session: false }),
+  async (req, res, next) => {
+    let userSchedule = await Schedule.findOne({
+      author: req.body.userId,
+    }).exec();
+
+    try {
+      let filteredArray = userSchedule.dates.filter(
+        (date) => date._id != req.body.dateId
+      );
+      console.log(filteredArray);
+
+      let savedSchedule = await Schedule.findOneAndUpdate(
+        { author: req.body.userId },
+        {
+          dates: filteredArray,
+        }
+      ).exec();
+
+      res.status(200).json({
+        response: "Date removed from schedule sucessfully",
+        schedule: savedSchedule,
+      });
     } catch (err) {
       return next(err);
     }
--- routes/schedules.js
+++ routes/schedules.js
@@ -8,4 +8,6 @@ router.post("/", schedule_controller.get_user_schedule);
 
 router.post("/add", schedule_controller.post_dateToSchedule);
 
+router.post("/remove", schedule_controller.remove_dateFromSchedule);
+
 module.exports = router;
